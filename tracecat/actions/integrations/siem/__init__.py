742722e94163d6e4f4509ecc93d964062e9a3c98
acumencyber <kevin.robertson@acumencyber.com>
2024-08-09 09:16:12 +0000

    "Remove DateTime Elastic Integration from __init__"

--- tracecat/actions/integrations/siem/__init__.py
+++ tracecat/actions/integrations/siem/__init__.py
@@ -1,10 +1,8 @@
 from .datadog import list_datadog_alerts
 from .elastic import list_elastic_alerts
 from .acumen_elastic import acumen_list_elastic_alerts
-from .acumen_elastic_datetime import acumen_list_elastic_alerts_datetime
 __all__ = [
     "list_datadog_alerts",
     "list_elastic_alerts",
     "acumen_list_elastic_alerts"
-    "acumen_elastic_datetime"
 ]
